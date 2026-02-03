9899a0412523168083dcd3476326c5c1ad13af48
Shivek-Sharma24 <shiveksharma24@gmail.com>
2026-02-03 22:23:22 +0530

    url changes

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,16 @@ dotenv.config();
 
 const app = express();
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+    {
+        origin:[
+            'https://pern-crud-project.vercel.app',
+            'https://pern-crud-project.onrender.com'
+        ],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+       allowedHeaders: ["Content-Type", "Authorization"]
+    }
+))
 app.use(helmet({
     contentSecurityPolicy: false,
 })) // helmet is a security middleware that helps to protect your app by setup various http headers. 
